8b8975e281efe402334c2fc115671f1088615cbe
Jean Michael <jeanmmclaudino@gmail.com>
2021-08-23 22:45:54 -0300

    first commit

---
+++ crud-app/database/migrations/2021_08_24_013102_cadastro.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Cadastro extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('cadastro', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome completo');
+            $table->string('rua');
+            $table->decimal('numero');
+            $table->string('bairro');
+            $table->string('cidade');
+            $table->string('estado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
